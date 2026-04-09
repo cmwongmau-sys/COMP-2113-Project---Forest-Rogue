5eddaecd405091c8fabd7cc543448a7d9af875a5
KEMICIOUS <winambekatibe@gmail.com>
2026-04-10 01:42:37 +0800

    Update random.h

--- random.h
+++ random.h
@@ -1,2 +1,10 @@
 #ifndef EVENTS_H
 #define EVENTS_H
+
+void wildAnimalEncounter(int choice, int difficulty, int &health, int &food, int &water)
+
+void treasureEncounter(int choice, int difficulty, int &health, int &food, int &water)
+
+void trapEncounter(int choice, int difficulty, int &health, int &food, int &water)
+
+#endif
