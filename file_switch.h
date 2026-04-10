e4c614b1e459315b20ad4743fb929789f6a2a248
Justin-H921 <hungkwokkiu921@gmail.com>
2026-04-11 00:30:53 +0800

    Update file_switch.h

--- file_switch.h
+++ file_switch.h
@@ -1,1 +1,24 @@
+#ifndef FILE_SWITCH_H
+#define FILE_SWITCH_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
+//scoreboard column
+struct ScoreEntry 
+    string name;
+    int difficulty;
+    int finalScore;
+    int excessFood;
+    int excessWater;
+    int zonesCompleted;
+    string dateTime;
+};
+
+// Function declarations
+void saveScore(ScoreEntry entry);
+void loadScoreboard(vector<ScoreEntry>& scoreboard);
+void displayTop10(const vector<ScoreEntry>& scoreboard);
+
+#endif
