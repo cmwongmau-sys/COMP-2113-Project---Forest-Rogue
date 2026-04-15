4125a0e2face1a9cba0442fec4e3d5a34e7f24c1
Wong Maurice <wongmaurice@Wongs-MacBook-Air-2.local>
2026-04-16 00:43:29 +0800

    Integrated the file I/O into the main program

--- main.cpp
+++ main.cpp
@@ -4,26 +4,60 @@
 #include <string>
 #include <cstdlib>  // for rand() and srand()
 #include <ctime>    // for time()
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include "file_switch.h"
 using namespace std;
 
 int main() {
 
-    // Call the function to print screen
+    // Call the function to print welcome screen
 
     // Read the user's input of his name
     string name;
     getline(cin, name);
+
+    // Ask the use if he have files to input (Y/N)
+
+
+    // Read the user's input of his answer and file
+    string ans, file;
+    cin >> ans;
+
+    if (ans == "Y") {
+        // Get the file path
+        getline(cin, file);
+        vector<ScoreEntry> scoreboard;
+        
+        // load the data into the scoreboard
+        loadScoreboard(scoreboard, file);
+    }
 
     // Read the user's input for the difficulty level
     // 0/1/2 for easy/medium/hard
     int mode;
     cin >> mode;
 
-    // Call the function to print screen
+    // Ask the user if he has any file to input
+    char ans;
+    cout << "Do you have any player statistics to input? (Y/N)" << endl;
+    cin >> ans;
+
+    string path;
+    if (ans == 'Y') {
+        cout << "Please give the path of the file for player statistics: ";
+        cin >> path;
+        cout << endl;
+    }
+
+    // Call the function to print start screen
 
 
     // Enter the game and go through 6 zones
-    for (int zone = 1; zone < 7; zone ++) {
+    int zone = 1;
+    for (; zone < 7; zone ++) {
 
         int health = 100, food, water;
 
@@ -100,9 +134,31 @@ int main() {
 
     // Display victory screen
     // Calculate the score of the player including the bonus from excess food and water
+    int score;
+
     // Display scoreboard
 
+    // File output
+    // Get current time
+    auto now = chrono::system_clock::now();
+    time_t currentTime = chrono::system_clock::to_time_t(now);
+    
+    // Convert to local time
+    tm* localTime = localtime(&currentTime);
 
+    // Datetime Format 1: For filename
+    stringstream ss_filename;
+    ss_filename << put_time(localTime, "%Y-%m-%d-%H%M%S");
+    string dateTimeFilename = ss_filename.str();
+    string filename = "scoreboard_" + dateTimeFilename + ".txt"
+
+    // Datetime Format 2: For player data
+    stringstream ss_data;
+    ss_data << put_time(localTime, "%Y-%m-%d %H:%M:%S");
+    string dateTimeData = ss_data.str();
+    ScoreEntey player = {name, mode, score, food, water, zone, dateTimeData};
+
+    saveScoreboard(scoreboard, filename);
 
     return 0;
 }
