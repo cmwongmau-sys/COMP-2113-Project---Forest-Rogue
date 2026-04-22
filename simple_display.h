09fa3c790f8615108f9110bc1e7483650301768d
Wong Maurice <cmwong.mau@gmail.com>
2026-04-23 01:25:37 +0800

    revised the libraries and the input parameters

--- simple_display.h
+++ simple_display.h
@@ -1,7 +1,10 @@
 #ifndef SIMPLE_DISPLAY_H
 #define SIMPLE_DISPLAY_H
 
+#include <string>
+#include <vector>
+
 void startScreen();
-void DrawStaticFrame(const string& bannerText, const vector<string>& content);
+void DrawStaticFrame(const std::string& bannerText, const std::vector<std::string>& content);
 
 #endif // SCREEN_DISPLAY_H
