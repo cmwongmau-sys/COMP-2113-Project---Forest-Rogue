7db2c1e3f5482c4524d53f881ac28a072588e5f3
Wong Chun Ming Maurice <cmwong.mau@gmail.com>
2026-04-26 01:26:22 +0800

    debugged for array assignment

--- event_generation.cpp
+++ event_generation.cpp
@@ -4,35 +4,45 @@
 #include <string>
 #include <cstdlib>  // for rand() and srand()
 #include <ctime>    // for time()
+#include <array>
 #include "event_generation.h"
 using namespace std;
 
 // Generate the random event based on the probability of difficulty modes
 int randomEvent(int mode) {
 
-    // Upper bound array for 7 events , ignore index 0
-    std::array<int, 8> upperBound = {-1, 20, 40, 50, 65, 80, 90, 100};
-
-    switch (mode) {
-        case 0:  // Easy
-            upperBound = {-1, 20, 40, 50, 65, 80, 90, 100};
-            break;
-        case 1:  // Medium
-            upperBound = {-1, 25, 40, 55, 65, 75, 90, 100};
-            break;
-        case 2:  // Hard
-            upperBound = {-1, 30, 40, 60, 65, 70, 90, 100};
-            break;
-    }
-
     // Generate the random number
     srand(time(0));
     int randomNum = rand() % 100 + 1;
 
-    // Find the corresponding random event
-    for (int i = 1; i < 8; i++) {
-        if (randomNum <= upperBound[i])
-            return i;
+    switch (mode) {
+        case 0: { // Easy
+            std::array<int, 8> upperBound = {-1, 20, 40, 50, 65, 80, 90, 100};
+            // Find the corresponding random event
+            for (int i = 1; i < 8; i++) {
+                if (randomNum <= upperBound[i])
+                    return i;
+            }
+            break;
+        }
+        case 1: { // Medium
+            std::array<int, 8> upperBound = std::array<int, 8>{-1, 25, 40, 55, 65, 75, 90, 100};
+            // Find the corresponding random event
+            for (int i = 1; i < 8; i++) {
+                if (randomNum <= upperBound[i])
+                    return i;
+            }
+            break;
+        }
+        case 2: { // Hard
+            std::array<int, 8> upperBound = std::array<int, 8>{-1, 30, 40, 60, 65, 70, 90, 100};
+            // Find the corresponding random event
+            for (int i = 1; i < 8; i++) {
+                if (randomNum <= upperBound[i])
+                    return i;
+            }
+            break;
+        }
     }
 
     return 7;
